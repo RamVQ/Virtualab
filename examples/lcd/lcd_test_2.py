641d30236e6bb420b6e6a675426678d50b5079b1
RamVQ <rvq25@hotmail.com>
2017-11-22 22:06:25 -0600

    1

--- examples/lcd/lcd_test_2.py
+++ examples/lcd/lcd_test_2.py
@@ -55,7 +55,7 @@
 import RPi.GPIO as GPIO
 import time
 
-# Definición de los pines datos/control del LCD
+# Definición de los pines datos control del LCD
 # Define GPIO to LCD mapping
 LCD_RS = 7
 LCD_E  = 8
