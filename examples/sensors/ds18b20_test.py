0b4eddf2b4fbe4e46c1d06f0bb7b4c65664b2e9c
RamVQ <rvq25@hotmail.com>
2017-11-27 00:26:28 -0600

    updated thermal sensor

--- examples/sensors/ds18b20_test.py
+++ examples/sensors/ds18b20_test.py
@@ -2,18 +2,15 @@ import os
 import glob
 import time
  
-#os.system('modprobe w1-gpio')
-#os.system('modprobe w1-therm')
- 
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
-print "Device_file="+device_file
+print ("Device_file=")+device_file
 
  
 def read_temp_raw():
-    f = open(device_file, 'r')
-    lines = f.readlines()
+   	f = open(device_file, 'r')
+   	lines = f.readlines()
 	f.close()
 	return lines
      
@@ -22,7 +19,7 @@ def read_temp():
 	while lines[0].strip()[-3:] != 'YES':
 		time.sleep(0.2)
 		lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
+	equals_pos = lines[1].find('t=')
 	if equals_pos != -1:
 		temp_string = lines[1][equals_pos+2:]
 		temp_c = float(temp_string) / 1000.0
@@ -30,5 +27,5 @@ def read_temp():
 		return temp_c, temp_f
     	
 while True:
-	print(read_temp())	
+	print("Temperature(C,F)="+str(read_temp()))	
 	time.sleep(1)
